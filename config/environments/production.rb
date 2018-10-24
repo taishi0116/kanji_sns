1278d5459285ecdf65c077182d748168c5df50ab
Taishi <bobochin16@gmail.com>
2018-10-24 11:48:18 +0000

    rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "kanji-SNS_#{Rails.env}"
 config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = '<6wordssns>.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
